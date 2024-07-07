b5330a7bca1104b0aab0a612416a08ab7da9de06
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-07-07 11:29:41 +0530

    function update

---
+++ functions/fun1.java
@@ -0,0 +1,19 @@
+// note :- all code write under the class  and in c++ all code write a outside the class.
+
+public class fun1 {
+    public static void HelloWorld(){
+
+    System.out.println("Hello World");
+    System.out.println("Hello world");
+    System.out.println("Hello world");
+
+    // return ;   mostly use for int  dont use for void data type.
+    }
+    public static void main (String args[]){
+        // call function
+       HelloWorld(); 
+
+
+    }
+    
+}
