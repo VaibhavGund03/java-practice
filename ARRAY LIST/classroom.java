b83a6deab615556b01cc7806bc440cee31a828e4
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-07-11 08:28:16 +0530

    update

---
+++ ARRAY LIST/classroom.java
@@ -0,0 +1,38 @@
+//---- array list is a inbult data structure in java.
+
+// linear structure.
+
+/* array ==
+
+ -- fixed size 
+ -- primitive data type can stored
+
+ arraylist ==
+ 
+ --dynamic size 
+--vprimitive data type cant be store directly
+
+ */
+
+import java.util.ArrayList;  // only sue for array list.
+
+// import java.util.*;  // acess all type class 
+
+public class classroom{
+    public static void main (String[]args){
+     // arraylist <datatype> arraylist name
+     
+     ArrayList <Integer>list = new ArrayList<>(); // call the constructure.
+     ArrayList<Boolean>list3 = new ArrayList<>(); 
+     // ArrayList work like Vector in 
+
+
+
+
+        
+
+    }
+
+
+    
+}
---
+++ recursion/rec1.java
@@ -0,0 +1,34 @@
+// recursion is process in which a function call itself  directly or indirectly is called recursion ===== and corrosponding function is called recursive function 
+/* use of =
+ 
+-- solve easly any problem 
+
+public static void main (String[args]){
+recurse() 
+}
+ 
+satic void recurse(){
+recurse()
+
+}
+                                   
+                                   
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+package recursion;
+
+public class rec1 {
+    
+}
