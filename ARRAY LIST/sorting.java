0d414f26fb59ad1f46d78debd90b64f135d691dc
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-07-23 20:06:09 +0530

    sorting assending dissending

--- ARRAY LIST/sorting.java
+++ ARRAY LIST/sorting.java
@@ -1,5 +1,24 @@
 /* */
-
+import java.util.ArrayList;
+import java .util.Collections;
 public class sorting {
+    public static void main (String args[]){
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        list.add(70);
+        list.add(80);
+        list.add(90);
+        list.add(10);
+        System.out.println(list);
+        Collections.sort(list); 
+        // to print the array sort in assending order 
+        System.out.println(list+"\n\n\n");
+
+        Collections.sort(list,Collections.reverseOrder());
+        System.out.println(list);
+
+    }
     
 }
