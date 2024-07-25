80e8ff60c93b4a08f5774ef84f8e5016dd0aaacf
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-07-25 21:26:55 +0530

    update

---
+++ LINKED LIST/linked1.java
@@ -0,0 +1,41 @@
+
+import java. util. LinkedList;
+
+public class linked1 {
+
+    public static class node{
+
+        int data;
+        node next;
+
+        public node(int data){
+
+            this.data = data;
+            this.next = null;
+
+        }
+
+    }
+
+   public static node head;
+   public static node tail;
+
+
+   // add()  remove()   search()   print()  
+
+   public static void main (String[]args){
+    
+    linked1 ll = new linked1();
+
+    // mostly i am not use this type process beacause i am used to meathods to perform to all operations.
+
+   /* 
+   ll.head = new node(1);
+    ll.head.next = new node(2);
+*/ 
+
+
+
+   }
+
+}
