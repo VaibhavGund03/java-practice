e88cc20e3ef21492683a374f47e2e02be0fa44c2
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-12-26 20:50:43 +0530

    update

--- loops/while1.java
+++ loops/while1.java
@@ -1,12 +1,35 @@
+import java.util.Scanner;
+
 public class while1 {
 
     public static void main(String[]args){
+          // declaration section
+           
+        int i =3;
         int a = 5;
-        while(a>=1){
+          // first loop 
+
+        while(a>=0){
             System.out.println(a);
             a--;
         }
 
+        // secon loop
+
+        while(i<3){
+            System.out.println(i);
+            i++;
+        }
+        //third loop
+
+        Scanner scanner = new Scanner(System.in); // Correct input handling
+        boolean isExit = false;
+        System.out.println("Type 'exist' to exit the loop:");
+        while (!isExit) {
+            String s = scanner.nextLine();
+            isExit = s.equals("exist");
+        }
+        scanner.close(); 
     }
-    
+
 }
