b12661d8d6790abc199c9ce7d1c6e0cd81a05b73
vaibhav gund <vaibhav.gcluster@gmail.com>
2024-12-26 08:34:49 +0530

    update

---
+++ loops/while1.java
@@ -0,0 +1,12 @@
+public class while1 {
+
+    public static void main(String[]args){
+        int a = 5;
+        while(a>=1){
+            System.out.println(a);
+            a--;
+        }
+
+    }
+    
+}
